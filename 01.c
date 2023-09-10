2e009b698356c7ccfd9d739453cdc13f70573915
DanielCoffy <daniel.danielcoffy@gmail.com>
2023-09-10 02:25:17 -0300

    Update 01.c

--- 01.c
+++ 01.c
@@ -4,9 +4,12 @@ Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
 *******************************************************************************/
 
 #include <stdio.h>
+#include <locale.h> //necessário para usar setlocale
+
 
 int main()
 {
+    setlocale(LC_ALL, "");
     printf("Olá, mundo!");
 
     return 0;
